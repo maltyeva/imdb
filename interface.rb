60c7965a95fba6769b8e9a963786af790b42a0b7
Maria <maria.altyeva@gmail.com>
2018-10-23 17:20:11 +0900

    added pseudo code

---
+++ interface.rb
@@ -0,0 +1,9 @@
+#we want to scrape the url for each of the 5 movies
+#create an array
+#store the urls in the array
+# we want to create a funtion that is going to find the information we need
+#scrape the information about the cast director storyline title and year
+#create a hash and store movie info
+#put 5 hashes into an array [5]
+#convert the information to yml
+#store the information to yml
